06705596fc272bcc16b1cb66668c596090e21470
spyD1803 <sainathbanuru96@gmail.com>
2016-12-16 13:34:51 +0530

    Added Links

--- portal/urls.py
+++ portal/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'',views.index,name="index"),
+    url(r'^$',views.index,name="index"),
+    url(r'^events/$',views.events,name='events'),
+    url(r'^almanac/$',views.almanac,name='almanac'),
+    url(r'^timetable/$',views.timetable,name='timetable'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -6,3 +6,10 @@ from django.http import HttpResponse
 def index(request):
     return render(request,'portal/index.html',{})
 
+def events(request):
+    return render(request,'portal/events.html',{})
+def almanac(request):
+    return render(request,'portal/almanac.html',{})
+def timetable(request):
+    return render(request,'portal/timetable.html',{})
+
